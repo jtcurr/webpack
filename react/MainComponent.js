67cfd8737dc9f0792a345542266e9cb35a4a3bb0
John Currie <johntcurrie@gmail.com>
2017-04-17 15:56:52 -0700

    Finished main component

--- react/MainComponent.js
+++ react/MainComponent.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import UsersComponent from './UsersComponent';
 
 export default class MainComponent extends Component {
-	
+
   constructor(props) {
   	super(props)
+  }
+
+  render() {
+  	return (
+  		<div>
+  		  <h1>Users</h1>
+        <UsersComponent />
+      </div>
+    )
   }
 }
