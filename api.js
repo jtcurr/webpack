489a0b0d19e4e03f938638d916928721bab91b9b
John Currie <johntcurrie@gmail.com>
2017-04-17 13:51:06 -0700

    Added webpack config file

---
+++ api.js
@@ -0,0 +1,22 @@
+module.exports = {
+	getUsers: function() {
+		return [
+			{
+		    name: 'Alex',
+		    dog: 'Bacos'		
+			},
+			{
+				name: 'Travis',
+				dog: 'Gil'
+			},
+      {
+      	name: 'Michael',
+      	dog: 'Ed'
+      },
+      {
+      	name: 'Mark',
+      	dog: 'Reka'
+      }
+		]
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+	entry: './app.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js'
+	}
+
+}
