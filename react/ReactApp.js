db3dbd71c43cf44dbe80d54accdcef6060af693a
John Currie <johntcurrie@gmail.com>
2017-04-17 16:00:08 -0700

    Added ReactApp component

---
+++ react/ReactApp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MainComponent from './MainComponent';
+
+const mountNode = document.getElementById('mountNode');
+render(<MainComponent />, mountNode);
