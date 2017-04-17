864de30a43a889d6c5c2c01a35262238b392f843
John Currie <johntcurrie@gmail.com>
2017-04-17 16:01:39 -0700

    Added mountNode div tags to html and required App.js in app

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const $ = require('jquery');
 const api = require('./api.js');
 const users = api.getFXRates();
 const welcomeUser = require('./welcomeUser.coffee');
+const ReactApp = require('./react/ReactApp.js');
 
 welcomeUser('Jones');
